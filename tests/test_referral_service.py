b0553938472d534e8da79ba97ce1386674955d65
Thomas Bosch <bosch-thomas@protonmail.com>
2024-09-09 20:34:44 +0200

    fix, code lint err, unused import

--- tests/test_referral_service.py
+++ tests/test_referral_service.py
@@ -1,7 +1,5 @@
 from unittest import TestCase
 
-from pyparsing import empty
-
 from app.config import set_config
 from app.data import UraNumber, Pseudonym, DataDomain
 from app.response_models.referrals import ReferralEntry
