164893b848f71f7b8522e8a07a71205290165155
Fouad Sfarijlani <fouad.sfarijlani@hotmail.com>
2026-01-05 14:14:00 +0100

    feat: initial Bundle implementation

---
+++ app/models/data_reference/bundle.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+from typing import List
+
+from pydantic import BaseModel, ConfigDict
+from pydantic.alias_generators import to_camel
+
+from app.models.data_reference.resource import (
+    NVIDataReferenceBase,
+    NVIDataReferenceOutput,
+)
+
+
+class BundleEntry(BaseModel):
+    model_config = ConfigDict(alias_generator=to_camel, populate_by_name=True)
+
+    resource: NVIDataReferenceBase | None = None
+    full_url: str | None = None
+
+
+class Bundle(BaseModel):
+    model_config = ConfigDict(alias_generator=to_camel, populate_by_name=True)
+
+    id: str | None = None
+    type: str = "searchset"
+    timestamp: datetime = datetime.now()
+    total: int | None = None
+    entry: List[BundleEntry]
+
+    @classmethod
+    def from_reference_outputs(cls, nvi_references: List[NVIDataReferenceOutput]) -> "Bundle":
+        entries = [BundleEntry(resource=nvi_ref) for nvi_ref in nvi_references]
+        obj = cls(entry=entries)
+        obj.total = len(obj.entry)
+
+        return obj
