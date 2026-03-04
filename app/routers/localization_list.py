7e7a1fdf0305e70aaad1df8c25e74e84163a9040
Fouad Sfarijlani <fouad.sfarijlani@hotmail.com>
2026-03-04 15:33:14 +0100

    feat: implementation of localiize endpoint for List

--- app/routers/localization_list.py
+++ app/routers/localization_list.py
@@ -1,17 +1,28 @@
+import base64
+import json
 import logging
-from typing import Any
+from typing import Annotated, Any
 
 from fastapi import APIRouter, Depends
+from fastapi.params import Query
 
 from app.dependencies import (
     get_localization_list_service,
+    get_pseudonym_service,
+    get_referral_service,
 )
 from app.exceptions.fhir_exception import FHIRException
+from app.models.data_domain import DataDomain
 from app.models.fhir.bundle import (
     Bundle,
+    BundleEntry,
 )
+from app.models.fhir.resources.localization_list.request import LocalizationListParams
 from app.models.fhir.resources.localization_list.resource import LocalizationList
+from app.models.pseudonym import Pseudonym
 from app.services.localization_list import LocalizationListService
+from app.services.prs.pseudonym_service import PseudonymService
+from app.services.referral_service import ReferralService
 
 logger = logging.getLogger(__name__)
 router = APIRouter(tags=["v1-alpha - FHIR"], prefix="/v1-alpha/fhir")
@@ -35,3 +46,69 @@ def register(
         result = localisation_list_service.process_entry(entry=entry, index=i)
         results_bundle.entry.append(result)
     return results_bundle
+
+
+@router.get("/List")
+def bla(
+    params: Annotated[LocalizationListParams, Query()],
+    referral_service: ReferralService = Depends(get_referral_service),
+    pseudonym_service: PseudonymService = Depends(get_pseudonym_service),
+) -> Any:
+    if params.patient is None and params.source is None:
+        raise FHIRException(
+            status_code=400,
+            severity="error",
+            code="invalid",
+            msg="Missing properties, either patient.identifier or source.identifier must be present",
+        )
+
+    code: str | None = None
+    pseudonym: Pseudonym | None = None
+    source: str | None = None
+
+    if params.patient:
+        try:
+            patient_identifier = params.get_patient_identifier()
+        except ValueError as e:
+            logger.error(f"error occurred while parcing query: {e}")
+            raise FHIRException(
+                status_code=400,
+                severity="error",
+                code="invalid",
+                msg="Malformed patient.identifier parameter",
+            )
+
+        try:
+            decoded_token = base64.urlsafe_b64decode(patient_identifier.value)
+            data = json.loads(decoded_token)
+            pseudonym = pseudonym_service.exchange(oprf_jwe=data["pseudonym"], blind_factor=data["oprfKey"])
+        except Exception as e:
+            logger.error(f"error occurred while decoding pseudonym token: {e}")
+            raise FHIRException(
+                status_code=400,
+                severity="error",
+                code="invalid",
+                msg="Invalid pseudonym in patient.identifier",
+            )
+
+    if params.source:
+        try:
+            source_identifier = params.get_source_identifier()
+            source = source_identifier.value
+        except ValueError as e:
+            logger.error(f"error occurred while parcing query: {e}")
+            raise FHIRException(
+                status_code=400,
+                severity="error",
+                code="invalid",
+                msg="Malformed source.identifier parameter",
+            )
+
+    referrals = referral_service.get_many(pseudonym=pseudonym, source=source, data_domain=DataDomain(code))
+    bundle = Bundle(
+        type="searchset",
+        total=len(referrals),
+        entry=[BundleEntry(resource=LocalizationList.from_referral(r)) for r in referrals],
+    )
+
+    return bundle
