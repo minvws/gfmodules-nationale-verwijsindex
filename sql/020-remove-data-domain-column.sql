7745b5049cc87c38162698e7d87c4ef4a83e9a06
Fouad Sfarijlani <fouad.sfarijlani@hotmail.com>
2026-05-19 15:58:04 +0200

    fix: migrations onn data_domain

--- sql/020-remove-data-domain-column.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE referrals DROP CONSTRAINT providers_unique_idx;
-ALTER TABLE referrals DROP COLUMN data_domain;
-ALTER TABLE referrals ADD CONSTRAINT providers_unique_idx UNIQUE (ura_number, pseudonym, source);
