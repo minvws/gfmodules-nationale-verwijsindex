1d9f0478fd944ea0777bac4af1763b705dd8b524
Fouad Sfarijlani <fouad.sfarijlani@hotmail.com>
2026-05-20 12:48:32 +0200

    cleanup: removed cert related functions with puzi library

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,7 @@
-import os
-import subprocess
 from collections.abc import Generator
-from pathlib import Path
-from typing import Any, Final
+from typing import Any
 
 import pytest
-from cryptography.x509 import Certificate
 
 from app.config import ConfigDatabase
 from app.db.db import Database
@@ -15,15 +11,7 @@ from app.models.ura import UraNumber
 from app.services.auth.header import AuthHeaderService
 from app.services.http import HttpService
 from app.services.referral_service import ReferralService
-from app.utils.certificates.utils import (
-    load_certificate,
-    load_one_certificate_file,
-)
 from tests.test_config import get_test_config
-
-TEST_SCRIPT_PATH: Final[str] = "tools/generate_test_certs.sh"
-TEST_CERTIFICATE_PATH: Final[str] = "tests/secrets/mock-server-cert.crt"
-TEST_CERTIFICATE_DIR: Final[str] = "tests/secrets/"
 
 
 @pytest.fixture()
@@ -78,37 +66,3 @@ def referral_service(database: Database) -> ReferralService:
 def auth_header_service() -> AuthHeaderService:
     config = get_test_config()
     return AuthHeaderService(expected_audience=config.authorization_headers.expected_audience)
-
-
-@pytest.fixture(scope="session")
-def certificate_str(ura_number: UraNumber) -> str:
-    global TEST_SCRIPT_PATH
-    global TEST_CERTIFICATE_PATH
-    root = Path(__file__).parent.parent
-    path = os.path.join(str(root) + "/" + TEST_SCRIPT_PATH)
-    exit_code = subprocess.call(f"{path} {str(ura_number)}", shell=True)
-    if exit_code != 0:
-        raise Exception(f"script {TEST_SCRIPT_PATH} exited with error code: {exit_code}")
-
-    return load_one_certificate_file(TEST_CERTIFICATE_PATH)
-
-
-@pytest.fixture()
-def certificate() -> Certificate:
-    global TEST_CERTIFICATE_PATH
-    return load_certificate(TEST_CERTIFICATE_PATH)
-
-
-@pytest.fixture()
-def cert_path() -> str:
-    global TEST_CERTIFICATE_PATH
-    path = TEST_CERTIFICATE_PATH
-
-    return path
-
-
-@pytest.fixture()
-def cert_dir() -> str:
-    global TEST_CERTIFICATE_DIR
-    dir = TEST_CERTIFICATE_DIR
-    return dir
